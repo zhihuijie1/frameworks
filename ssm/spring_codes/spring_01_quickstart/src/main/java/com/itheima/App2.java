8ee7a3c3482cfafb35177f377ce6e3dfeb4ed58b
yibai <404223879@qq.com>
2024-05-15 23:32:45 +0800

    spring

--- ssm/spring_codes/spring_01_quickstart/src/main/java/com/itheima/App2.java
+++ ssm/spring_codes/spring_01_quickstart/src/main/java/com/itheima/App2.java
@@ -7,9 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App2 {
     public static void main(String[] args) {
-        //3.获取IoC容器
+        //先获取IoC容器，然后再从容器中拿出bean
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //4.获取bean（根据bean配置id获取）
+        //获取bean（根据bean配置id获取）
 //        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
 //        bookDao.save();
 
