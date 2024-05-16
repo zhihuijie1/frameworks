0e0bd14bbd6e3ea5bd6cfb3f9bf9bde416f36aa2
yibai <404223879@qq.com>
2024-05-16 10:30:28 +0800

    factorybean

--- ssm/spring_codes/spring_03_bean_instance/src/main/java/com/itheima/AppForInstanceUser.java
+++ ssm/spring_codes/spring_03_bean_instance/src/main/java/com/itheima/AppForInstanceUser.java
@@ -9,13 +9,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppForInstanceUser {
     public static void main(String[] args) {
-//        //创建实例工厂对象
-//        UserDaoFactory userDaoFactory = new UserDaoFactory();
-//        //通过实例工厂对象创建对象
-//        UserDao userDao = userDaoFactory.getUserDao();
-//        userDao.save();
-
-
         //拿到IOC容器
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao userDao1 = (UserDao)ctx.getBean("userDao");
